0ecc387f4fc7c89221136ef3f249d7b99f291e28
Mpablom <pablomoya211@gmail.com>
2022-07-03 22:08:39 -0300

    solucion formulario

--- validacion.js
+++ validacion.js
@@ -6,6 +6,38 @@ const actualYear = new Date().getFullYear();
 
 document.getElementById("year").innerHTML = actualYear;
 
+//formulario
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSumit)
+async function handleSumit(event) {
+  event.preventDefault()
+  new FormData(this)
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    header : {
+      'Accept': 'application/json'
+    }
+  })
+  if(response.ok){
+    this.reset()
+    Swal.fire({
+      icon: 'success',
+      title: '¡Muchas Gracias!',
+      text: '¡Su mensaje se ha enviado correctamente, me contactaré con usted a la brevedad',
+      showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+  })
+  }
+}
+
 //validación.
 
 function validarDatos(){
@@ -77,17 +109,18 @@ function validarDatos(){
         })
         document.formulario.mensaje.focus();
     } else if(document.formulario.mensaje.value.length <= 500){
-        Swal.fire({
-            icon: 'success',
-            title: '¡Muchas Gracias!',
-            text: '¡Su mensaje se ha enviado correctamente, me contactaré con usted a la brevedad',
-            showClass: {
-                popup: 'animate__animated animate__fadeInDown'
-              },
-              hideClass: {
-                popup: 'animate__animated animate__fadeOutUp'
-              }
-        })
+      this.reset()
+    Swal.fire({
+      icon: 'success',
+      title: '¡Muchas Gracias!',
+      text: '¡Su mensaje se ha enviado correctamente, me contactaré con usted a la brevedad',
+      showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+  })     
     }else{
         Swal.fire({
             icon: 'error',
